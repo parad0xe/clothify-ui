4160e40cb07289d3104ddd52562705fbe5051abc
parad0xe <parad0xe@protonmail.com>
2023-04-29 13:11:16 +0200

    feature(Ajout d'une fonction de recherche communiquant avec l'api en spécifiant des termes de recherche, ajout d'une fonction qui retourne la configuration du trie pour spécifier comment créer le trie du model associé)

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,25 +1,42 @@
 import { Injectable } from '@angular/core';
 import { ProductResource } from "../resources/product.resource"
-import { Observable, of } from "rxjs"
+import { BehaviorSubject, Observable } from "rxjs"
 import ProductModel from "../../core/models/product.model"
 import ProductAttributModel from "../../core/models/productAttribut.model"
+import { Order, OrderConfigType, OrderSortCallback } from "../components/order/types/order-config.type"
+import { SearchTerms } from "../modules/search/search-term.class"
+
+export const SEARCH_PRODUCT_CONTEXT_KEY = "search:product"
 
 
 @Injectable()
 export class ProductService {
-	products$: Observable<ProductModel[]>
+	private _productSubject$$: BehaviorSubject<ProductModel[]> = new BehaviorSubject<ProductModel[]>([])
+	products$: Observable<ProductModel[]> = this._productSubject$$.asObservable()
 
-	constructor(productResource: ProductResource) {
-		this.products$ = productResource.all()
-	}
+	constructor(
+		private _productResource: ProductResource
+	) {}
 
 	get(id: number): Observable<ProductModel | undefined> {
-		return new Observable((observer) => {
-			this.products$.subscribe((products) => {
-				observer.next(products.find(product => product.id === id))
-				observer.complete()
-			})
-		})
+		return this._productResource.get(id)
+	}
+
+	findBy(terms: SearchTerms): Observable<ProductModel[]> {
+		return this._productResource.findBy(terms)
+	}
+
+	getOrderConfig(sortCallback: OrderSortCallback): OrderConfigType<ProductModel> {
+		return {
+			on: [
+				{ property: 'price', label: 'Prix' },
+				{ property: 'averageRating', label: 'Avis' }
+
+			],
+			selectedProperty: 'price',
+			selectedOrder: Order.ASC,
+			sort: sortCallback
+		}
 	}
 
 	getProductAttrsByCategoryName(product: ProductModel, name: string): ProductAttributModel[] {
